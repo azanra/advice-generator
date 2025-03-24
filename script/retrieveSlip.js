5adc45d104093005e214f499a8cbdf4de0689408
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-24 09:17:06 +0700

    Get advice from endpoint and display it to the dom

---
+++ script/retrieveSlip.js
@@ -0,0 +1,26 @@
+export default class RetrieveSlip {
+  slipId = document.querySelector("#adviceId");
+  advice = document.querySelector("#advice");
+
+  retrieveAdvice = () => {
+    const apiUrl = "https://api.adviceslip.com/advice";
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Response is not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.updateText(this.slipId, data.slip.id);
+        this.updateText(this.advice, data.slip.advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  updateText = (element, value) => {
+    element.textContent = value;
+  };
+}
